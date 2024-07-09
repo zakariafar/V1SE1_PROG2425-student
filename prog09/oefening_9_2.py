40e60152f4e66ad58e17e407ba5f8aa091253d9a
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2024-07-09 22:40:02 +0200

    Changed assignment

--- prog09/oefening_9_2.py
+++ prog09/oefening_9_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import traceback, collections, random
+import traceback, collections
 
 """
 Programming
@@ -17,29 +17,25 @@ Voeg commentaar toe om je code toe te lichten.
 """
 
 
-def monopolyworp():
+def wijzig(letterlijst):
     """
-    Simuleer het gooien van twee dobbelstenen voor het spel Monopoly. Als
-    beide stenen dezelfde waarde hebben, simuleer je nogmaals een worp. Na
-    driemaal dubbel moet de speler naar de gevangenis!
+    Deze functie heeft één parameter: letterlijst. Zorg dat de functie de lijst
+    leegt en de letters [ ‘d’, ‘e’, ‘f’ ] toevoegt aan de lijst.
 
-    Print de worpen èn return de gegooide worp(en) als lijst of tuple. In
-    de lijst plaats je per worp ook weer een tuple of lijst met de gegooide
-    waarden. Mogelijke returnwaarden van de functie:
-
-    [(1, 2)] - één keer gegooid, dus een lijst met één worp (als tuple)
-    [(4, 4), (4, 5)] - twee keer gegooid (eerste keer dubbel)
-    [(3, 3), (4, 4), (1, 1)] - drie keer gegooid (alle worpen dubbel)
-
+    Args:
+        letterlijst (list): Een lijst met een aantal losse karakters
     Returns:
-        list/tuple: verzameling van alle gesimuleerde worpen.
+        Er is geen return-waarde!
     """
     return
 
 
 def development_code():
     # Plaats hieronder eventueel code om je functies tussentijds te testen. Bijv:
-    print("Monopolyworp(en): ", monopolyworp())
+    lijst = ['a', 'b', 'c']
+    print(lijst)
+    wijzig(lijst)
+    print(lijst)
 
 
 def module_runner():
@@ -53,39 +49,53 @@ Hieronder staan de tests voor je code -- daaraan mag je niets wijzigen!
 """
 
 
-def __check_dices(result, dices):
-    msg = f"Fout: je functie monopolyworp() geeft wel een {type(result).__name__}, maar een worp moet daarin " \
-          f"weer als list of tuple geplaatst worden. Bijv. [(2,3)]. Functie monopolyworp() geeft nu: {result}"
-    assert isinstance(dices, (tuple, list)), msg
-    assert len(dices) == 2, f'Fout: per worp moeten twee dobbelstenen gegooid worden, nu gevonden: {dices}'
+def __my_assert_args(function, args, expected_output, check_type=False):
+    """
+    Controleer of gegeven functie met gegeven argumenten het verwachte resultaat oplevert.
+    Optioneel wordt ook het return-type gecontroleerd.
+    """
+    argstr = str(args).replace(',)', ')')
+    output = function(*args)
 
-    msg = "Fout: type van dobbelsteenwaarde {} moet int zijn, nu gevonden: {} in {}"
-    assert isinstance(dices[0], int), msg.format(dices[0], type(dices[0]).__name__, result)
-    assert isinstance(dices[1], int), msg.format(dices[1], type(dices[1]).__name__, result)
-    assert 0 < dices[0] <= 6, f'Fout: ongeldige dobbelsteen waarde: {dices[0]} in {result}'
-    assert 0 < dices[1] <= 6, f'Fout: ongeldige dobbelsteen waarde: {dices[1]} in {result}'
+    # Controleer eerst het return-type (optioneel)
+    if check_type:
+        msg = f"Fout: {function.__name__}{argstr} geeft geen {type(expected_output).__name__} terug als return-type"
+        assert type(output) is type(expected_output), msg
+
+    # Controleer of de functie-uitvoer overeenkomt met de gewenste uitvoer
+    if str(expected_output) == str(output):
+        msg = f"Fout: {function.__name__}{argstr} geeft {output} ({type(output).__name__}) " \
+              f"in plaats van {expected_output} (type {type(expected_output).__name__})"
+    else:
+        msg = f"Fout: {function.__name__}{argstr} geeft {output} in plaats van {expected_output}"
+
+    if type(expected_output) is float and isinstance(output, (int, float, complex)):
+        # Vergelijk bij float als return-type op 7 decimalen om afrondingsfouten te omzeilen
+        assert round(output - expected_output, 7) == 0, msg
+    else:
+        assert output == expected_output, msg
 
 
-def test_monopolyworp():
-    for i in range(1000):
-        result = monopolyworp()
-        assert isinstance(result, (tuple, list)), f"Fout: monopolyworp() levert {result} ipv een tuple of list op!"
+def test_wijzig():
+    case = collections.namedtuple('case', 'characterlist expected_output')
 
-        assert len(result) > 0, f'Fout: monopolyworp geeft {result}, maar er moet minstens één keer een worp plaatsvinden!'
-        __check_dices(result, result[0])
+    testcases = [ case(['a', 'b', 'c'], None),
+                  case([], None) ]
 
-        if result[0][0] == result[0][1]:
-            assert len(result) > 1, 'Fout: na de eerste keer dubbelgooien, moet nog een keer gegooid worden! {}'.format(result)
-            __check_dices(result, result[1])
+    for test in testcases:
+        tmp_list = test.characterlist.copy()
 
-            if result[1][0] == result[1][1]:
-                assert len(result) == 3, 'Fout: na de tweede keer dubbelgooien, mag niet nog een keer gegooid worden! Je functie geeft: {}'.format(result)
-                __check_dices(result, result[2])
+        __my_assert_args(wijzig, (test.characterlist,), test.expected_output)
+
+        check = test.characterlist == ['d', 'e', 'f']
+        msg = f"Fout: Na functie wijzig({tmp_list}) bevat de meegegeven lijst {test.characterlist} in plaats van {['d', 'e', 'f']}"
+
+        assert check, msg
 
 
 def __run_tests():
     """ Test alle functies. """
-    test_functions = [ test_monopolyworp ]
+    test_functions = [ test_wijzig ]
 
     try:
         for test_function in test_functions:
